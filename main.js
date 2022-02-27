d172a2310a0c62602b45051185a07f6c2f07d450
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-02-26 22:03:45 -0300

    added styles

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 window.addEventListener("keydown", function (e) {
 	const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
+	const drumLetter = document.querySelector(
+		`kbd[class ='drum-button ${e.keyCode}']`
+	);
+	const drumButton = document.querySelector(
+		`div[class ='button ${e.keyCode}']`
+	);
+
 	audio.currentTime = 0;
 	if (audio) {
 		audio.play();
+		drumLetter.classList.add("focus-letter");
+		drumButton.classList.add("focus-div");
+		setTimeout(() => {
+			drumLetter.classList.remove("focus-letter");
+		}, 140);
+		setTimeout(() => {
+			drumButton.classList.remove("focus-div");
+		}, 70);
 	}
 	console.log(audio);
 });
